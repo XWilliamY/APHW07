82d3a90df840b370c63e38d438464d0b97cf5359
William <xwilliamy@gmail.com>
2015-02-06 23:55:48 -0500

    sqrt function not working

--- 01Miracles/Recursion.java
+++ 01Miracles/Recursion.java
@@ -1,7 +1,8 @@
 public class Recursion implements hw1{
     public static void main(String[]args){
 	System.out.println(fact(19));
-	System.out.println(fib(7));
+	System.out.println(fib(2));
+	System.out.println(sqrt(4));
     }
 
     public String name(){
@@ -41,9 +42,16 @@ public class Recursion implements hw1{
 	//1 + 1 + 1 + 1 + 1 = 5 
     }
 
-    public double sqrt(double n){
-	return 1;
+    public static double sqrt(double n){
+	return sqrt2(n, 1);
+    }
+
+
+    public static double sqrt2(double n, double guess){
+	if((guess*guess)/n <= .01){
+	    return guess;
+	}
+	return sqrt2(n, (n/guess + guess)/2);
     }
 }
-
 
