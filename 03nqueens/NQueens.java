8a30b52e7b61890881d9a34f407bc1aadbcd911a
William <xwilliamy@gmail.com>
2015-02-26 21:25:16 -0500

    Updated files

--- 03nqueens/NQueens.java
+++ 03nqueens/NQueens.java
@@ -4,10 +4,28 @@ public class NQueens{
 
     public NQueens(int size){
 	board = new char [size][size];
+	//the way NQueens works is a lot like a maze in that
+	//once you set a Queen, there are certain spaces you can and cannot fill
+	//subsequently, we should document this change
+	//the best way to do so would be to fill in the board
+	//and as we lay down queens edit the characters in them
+	//so as to signify which spaces can and cannot be used 
+	for(int a = 0; a < board.length; a++){
+	    for(int b = 0; b < board[0].length; b++){
+		board[a][b] = '.';
+	    }
+	}
     }
 
     public String toString(){
-	return "yes";
+	String ans = "";
+	for(int a = 0; a < board.length; a++){
+	    for(int b = 0; b < board[0].length; b++){
+		ans += board[a][b] + " ";
+	    }
+	    ans += "\n";
+	}
+	return ans;
     }
 
     public String name(){
@@ -15,11 +33,27 @@ public class NQueens{
     }
 
     public boolean solve(){
+	//the first queen will be put down at zero
+	//we'll cycle through the board to see where we can put others
+	board[0][0] = '@';
+	//block off the other parts 
+	for(int a = 0; a < board.length; a++){
+	    if(!(board[a][0] == '@')){
+		board[a][0] = 'i';
+	    }
+	    for(int b = 0; b < board[0].length;b++){
+		if(!(board[0][b] == '@')){
+		    board[0][b] = 'i';
+		}
+	    }
+	}
 	return true;
     }
 
-    public boolean solve(int x){
-	return true;
+    
+    public static void main(String[] args){
+	NQueens a = new NQueens(10);
+	a.solve();
+	System.out.println(a.toString());
     }
-
 }
