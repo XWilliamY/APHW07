011bb729e79cc2c886b5117d569d46d74fd43594
William <xwilliamy@gmail.com>
2015-02-26 20:43:00 -0500

    NQueens folder made

---
+++ 03nqueens/NQueens.java
@@ -0,0 +1,25 @@
+public class NQueens{
+
+    private char [][] board;
+
+    public NQueens(int size){
+	board = new char [size][size];
+    }
+
+    public String toString(){
+	return "yes";
+    }
+
+    public String name(){
+	return "last.first";
+    }
+
+    public boolean solve(){
+	return true;
+    }
+
+    public boolean solve(int x){
+	return true;
+    }
+
+}
