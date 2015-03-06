809b63bcc9ccdf30bda8ba5ecff3f33e2d503e31
Your Name <xwilliamy@gmail.com>
2015-03-06 13:11:42 -0500

    06 quickselect folder made

---
+++ 06quicksort/split.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class split{
+
+    public void partition(int[]ary, int si, int ei){
+	int [] D = new int [ary.length];
+	int start = si;
+	int end = ei;
+	Random rand = new Random();
+
+	for(int a = 0; a < D.length; a++){
+	    if(a < si || a > ei){
+		D[a] = ary[a];
+	    }
+	}
+
+	int ri = si + (rand.nextInt(ei-si+1));
+	int pivot = ary[ri];
+
+	for( int i = start; i <= end; i++){
+	    System.out.println(Arrays.toString(D));
+	    if(ary[i] < pivot){
+		D[si] = ary[i];
+		si++;
+	    }
+	    else if (ary[i] > pivot){
+		D[ei] = ary[i];
+		ei--;
+	    }
+	}
+	D[si] = pivot;
+	System.out.println(pivot);
+	System.out.println(Arrays.toString(D));
+    }
+
+    public split(){
+	int [] ary = new int[10];
+	for(int i = 0 ; i < ary.length; i++){
+	    ary[i] = 10-i;
+	}
+
+	partition(ary, 2, 9);
+    }
+
+    public static void main(String[]args){
+	split A = new split();
+    }
+}
