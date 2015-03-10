5cb36de11a5abda53f442835281f26030896d4a4
William <xwilliamy@gmail.com>
2015-03-10 00:40:08 -0400

    It compiles. Includes partition and quicksort

--- 06quicksort/Sorts.java
+++ 06quicksort/Sorts.java
@@ -2,4 +2,59 @@ import java.util.*;
 
 public class Sorts{
 
+    public static void quickSort(int[]ary){
+	//make a call to partition 
+	quickSort(ary, 0, ary.length-1);
+    }
+
+    public static void quickSort(int[]ary, int si, int ei){
+	if(si < ei){
+	    int pi = partition(ary, si, ei);
+	    //boundary will be the pivot point
+	    //being either the lesser or greater limit 
+	    quickSort(ary, 0, pi - 1); //the left side
+	    quickSort(ary, pi + 1, ei); // the right side
+	}
+    }
+
+    public static int partition(int[]ary, int si, int ei){
+	int[] D = new int [ary.length];
+	int start = si;
+	int end = ei;
+	Random rand = new Random();
+	
+	for(int a = 0; a < D.length; a++){
+	    if(a < si || a > ei){
+		D[a] = ary[a];
+	    }
+	}
+	
+	int ri = si + (rand.nextInt(ei-si+1));
+	int pivot = ary[ri];
+	
+	for(int i = 0; i < ary.length;i++){
+	    if(ary[i] < pivot){
+		D[si] = ary[i];
+		si++;
+	    }
+	    else if (ary[i] > pivot){
+		D[ei] = ary[i];
+		ei--;
+	    }
+	}
+	
+	ri = si;
+	int pivotpoint = ri;
+	while(si <= ei){
+	    D[si] = pivot;
+	    si++;
+	}
+
+	return pivotpoint;
+    }
+
+    public static void main(String[]args){
+    }
 }
+
+
