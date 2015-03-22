396cd3ea04c34ec2411273ffda6b0a965845da0e
William <xwilliamy@gmail.com>
2015-03-22 10:42:09 -0400

    folder 08freepoints made, copied MyLinkedList.java and LNode.java over

---
+++ 08freepoints/LNode.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class LNode<T>{
+
+    private T value;
+    private LNode<T> next;
+
+    public String name(){
+	return "yang.william";
+    }
+
+    public LNode(T v){
+	value = v;
+    }
+
+    public LNode(){
+    }
+
+    public LNode<T> getNext(){
+	return next;
+    }
+
+    public T getValue(){
+	return value;
+    }
+
+    public void setValue(T v){
+	value = v;
+    }
+
+    public void setNext(LNode<T> n){
+	next = n;
+    }
+
+    public String toString(){
+	return "" + getValue();
+    }
+
+    public static void main(String[]args){
+	LNode<Integer> A = new LNode<Integer>();
+	A.setValue(3);
+	System.out.println(A.toString());
+	System.out.println(A.getNext());
+	LNode<Integer> B = new LNode<Integer>();
+	B.setValue(4);
+	A.setNext(B);
+	System.out.println(A.getNext());
+    }
+}
