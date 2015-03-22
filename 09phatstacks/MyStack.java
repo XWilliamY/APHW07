95eb0d1bc67b24f266567f2a531d27d492aa28c6
William <xwilliamy@gmail.com>
2015-03-22 10:33:49 -0400

    Grappled with nullpointerexception, decided to instantiate LinkedList<T> stack to avoid this issue

--- 09phatstacks/MyStack.java
+++ 09phatstacks/MyStack.java
@@ -1,15 +1,14 @@
 import java.util.*;
 
 public class MyStack<T>{
-
-    private LinkedList<T> tack;
+    private LinkedList<T> stack = new LinkedList<T>();
 
     public String name(){
 	return "yang.william";
     }
 
     public T push(T value){
-	tack.add(0, value);
+	stack.addFirst(value);
 	return value;
     }
 
@@ -29,8 +28,14 @@ public class MyStack<T>{
 
     public static void main(String[]args){
 	MyStack<Integer> A = new MyStack<Integer>();
-	A.push(5);
-	A.push(6);
-	System.out.println(A.toString());
+	System.out.println(A.push(5));
+	System.out.println(A.push(6));
+	System.out.println(A.peek());
+	System.out.println(A.pop());
+	System.out.println(A.pop());
+	//System.out.println(A.pop());
+	System.out.println(A.empty());
+	System.out.println(A.push(10));
+	System.out.println(A.peek());
     }
 }
