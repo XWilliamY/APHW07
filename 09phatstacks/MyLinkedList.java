acdb7b169610e0542b8e4b6f8c4a29e9b784d3c2
Your Name <xwilliamy@gmail.com>
2015-03-23 12:55:31 -0400

    Using MyLInkedList instead of LinkedList for MyStack

---
+++ 09phatstacks/MyLinkedList.java
@@ -0,0 +1,193 @@
+import java.util.*;
+
+public class MyLinkedList<T> implements Iterable<T>{
+    private class MyLLIterator<T> implements Iterator<T>{
+	private LNode<T> current;
+
+	public MyLLIterator(LNode<T> head){
+	    current = head;
+	}
+
+	public boolean hasNext(){
+	    return current != null;
+	}
+	
+	public T next(){
+	    if(hasNext()){
+		T ans = current.getValue();
+		current = current.getNext();
+		return ans;
+	    }
+	    throw new NoSuchElementException();
+	}
+
+	public void remove(){
+	    throw new UnsupportedOperationException();
+	}
+    }
+
+    public Iterator<T> iterator(){
+	return new MyLLIterator<T>(head);
+    }
+
+
+    private LNode<T> head;
+    private LNode<T> current;
+    private LNode<T> tail;
+    private int size = 0;
+    private int counter;
+  
+    public String name(){
+	return "yang.william";
+    }
+
+    public String toString(){
+	String ans = "[ ";
+	LNode<T> temp = head;
+	while(temp != null){
+	    ans += temp.getValue() + ",";
+	    temp = temp.getNext();
+	}
+	ans = ans.substring(0, ans.length()-1);
+	ans += " ]";
+	return ans;
+    }
+
+    public T get(int index){
+	if(index < 0 || index >= size){
+	    throw new IndexOutOfBoundsException();
+	}
+
+	counter = 0;
+	LNode<T> temp = head;
+	while(counter < index){
+	    temp = temp.getNext();
+	    counter++;
+	}
+	return temp.getValue();
+    }
+
+    public void set(int index, T value){
+	if(index < 0 || index >= size){
+	    throw new IndexOutOfBoundsException();
+	}
+
+	int counter = 0;
+	LNode<T> temp = head;
+
+	while(counter < index){
+	    temp = temp.getNext();
+	    counter++;
+	}
+	temp.setValue(value);
+    }
+
+    public boolean add(T value){
+	LNode<T> temp = head;
+
+	if(size == 0){
+	    head = new LNode<T>(value);
+	    tail = head;
+	}
+
+	else{
+	    LNode<T> addOn = new LNode<T>(value);
+	    tail.setNext(addOn);
+	    tail = addOn;
+	}
+	size++;
+	return true;
+    }
+
+    public void add(int index, T value){
+
+	if(index < 0 || index >= size && size > 0){
+	    throw new IndexOutOfBoundsException();
+	}
+
+	if(index == 0){
+	    LNode<T> addNode = new LNode<T>(value);
+	    addNode.setNext(head);
+	    head = addNode;
+	}
+	else{
+	    LNode<T> temp = head;
+	    for(int i = 0; i < index-1; i++){
+		temp = temp.getNext();
+	    }
+	    LNode<T> addNode = new LNode<T>(value);
+	    addNode.setNext(temp.getNext());
+	    temp.setNext(addNode);
+	}
+	size++;
+    }
+
+    public boolean remove(int index){
+	LNode<T> temp = head;
+	if(index < 0 || index >= size){
+	    throw new IndexOutOfBoundsException();
+	}
+	if(index == 0){
+	    head = head.getNext();
+	}
+	else{
+	    for(int i = 0; i < index-1;i++){
+		temp = temp.getNext();
+	    }
+	    try{
+		temp.setNext(temp.getNext().getNext());
+	    }catch(NullPointerException e){
+		temp.setNext(null);
+		tail = temp;
+	    }
+	}
+	size--;
+	return true;
+    }
+
+    public int size(){
+	return size;
+    }
+
+    public int indexOf(T value){
+	LNode<T> temp = head;
+	for(int i = 0; i < size; i++){
+	    if(temp.getValue() == value){
+		return i;
+	    }
+	    temp = temp.getNext();
+	}
+	return -1;
+    }
+
+
+    public static void main(String[]args){
+	MyLinkedList<String> A = new MyLinkedList<String> ();
+	A.add("Hello");
+	A.add("Goodbye");
+	A.add("We are testing");
+	A.add("see if this works");
+	/*System.out.println(A.toString());
+	A.remove(0);
+	System.out.println("removed Hello. linkedlist is now: " + A.toString());
+	A.set(1, "testing testing");
+	System.out.println("replaced 'we are testing' with 'testing testing'; linked list: " + A.toString());
+	A.add(2, "hellothere");
+	System.out.println("Added 'something' between testing testing, and see if this works. Linkedlist: " + A.toString());
+	System.out.println(A.get(0));
+	System.out.println(A.get(1));
+	System.out.println(A.get(2));
+	System.out.println(A.get(3));
+	//this properly leads to exception: System.out.println(A.get(4));
+	//also triggers exception A.remove(10);
+	//no swag: triggers exception: A.set(100, "swag");
+	*/
+	Iterator<String> m = A.iterator();
+	System.out.println(m.next());
+	System.out.println(m.next());
+	System.out.println(m.next());
+	System.out.println(m.next());
+    }
+
+}
+
--- 09phatstacks/MyStack.java
+++ 09phatstacks/MyStack.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
 public class MyStack<T>{
-    private LinkedList<T> stack = new LinkedList<T>();
+    private MyLinkedList<T> stack = new MyLinkedList<T>();
 
     public String name(){
 	return "yang.william";
     }
 
     public T push(T value){
-	stack.addFirst(value);
+	stack.add(0, value);
 	return value;
     }
 
@@ -28,14 +28,13 @@ public class MyStack<T>{
 
     public static void main(String[]args){
 	MyStack<Integer> A = new MyStack<Integer>();
-	System.out.println(A.push(5));
-	System.out.println(A.push(6));
+	System.out.println(A.push(10));
+	System.out.println(A.push(11));
+	System.out.println(A.push(12));
 	System.out.println(A.peek());
 	System.out.println(A.pop());
+	System.out.println(A.peek());
 	System.out.println(A.pop());
-	//System.out.println(A.pop());
-	System.out.println(A.empty());
-	System.out.println(A.push(10));
 	System.out.println(A.peek());
     }
 }
