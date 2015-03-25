db418a1eabfe75adf21a5b367fb4e91617d77725
William <xwilliamy@gmail.com>
2015-03-24 20:43:37 -0400

    folder 10 made

---
+++ 10DBQ/MyStack.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+public class MyStack{
+
+    private int [] stack;
+    private int head;
+    private int tail;
+    private int maxSize;
+    private int size = 0;
+    private int index = 0;
+
+    public MyStack(int size){
+	stack = new int [size];
+	maxSize = size;
+    }
+    
+    public MyStack(){
+	stack = new int [10];
+	maxSize = 10;
+    }
+
+    public int push(int value){
+	if((size + 1) > maxSize){
+	    int [] newStack = new int [2 * (size + 1)]; // make it 2x size
+	    for(int i = 0; i < stack.length; i++){
+		newStack[i] = stack[i];
+	    }
+	    stack = newStack;
+	}
+	index = size;
+	stack[index] = value; //add value
+	size ++; //increment counter
+	return value; 
+    }
+
+    public int pop(){
+	if(size == 0){
+	    throw new EmptyStackException();
+	}
+	int removed = stack[index];
+	index --; // moving back one treats the former as an empty space
+	size --;	
+	return removed;    
+    }
+
+    public int peek(){
+	if(size == 0){
+	    throw new EmptyStackException();
+	}
+	return stack[index];
+    }
+
+    public boolean empty(){
+	return size == 0;
+    }
+
+    public int getSize(){
+	return size;
+    }
+
+    public int getIndex(){
+	return index;
+    }
+
+    public static void main(String[]args){
+	MyStack A = new MyStack(1);
+	A.push(5);
+	A.push(6);
+	System.out.println(A.peek());
+    }
+}
+
+
+
