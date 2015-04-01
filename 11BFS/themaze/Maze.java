66706e71082b3332db43c45f3260e5fdb9a5fa01
Your Name <xwilliamy@gmail.com>
2015-04-01 13:12:03 -0400

    Frontier

--- 11BFS/themaze/Maze.java
+++ 11BFS/themaze/Maze.java
@@ -2,7 +2,6 @@ public class Maze{
     private char [][] ary = new char [10][10];
 
     public class Frontier{
-	MyDeque<Coordinate> frontier = new MyDeque<Coordinate>();
 	public class Coordinate{
 	    private int row, col;
 	    public void setRow(int row){
@@ -18,5 +17,17 @@ public class Maze{
 		return col;
 	    }
 	}
-    
+	public Frontier(){
+	    MyDeque<Coordinate> frontier = new MyDeque<Coordinate>();	    
+	    //if valid, make a coordinate and then add it to frontier
+	    for(int i = 0; i < ary.length; i++){
+		for(int a = 0; a < ary[0].length;a++){
+		    Coordinate A = new Coordinate();
+		    A.setRow(i);
+		    A.setCol(a);
+		}
+		frontier.addFirst(A);
+	    }
+	}
+    }
 }
