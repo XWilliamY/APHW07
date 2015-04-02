7c522252a7310885130d6b1961a187fd317facaf
Your Name <xwilliamy@gmail.com>
2015-04-02 13:04:25 -0400

    Frontier class aok

--- 11BFS/themaze/Maze.java
+++ 11BFS/themaze/Maze.java
@@ -1,5 +1,18 @@
 public class Maze{
     private char [][] ary = new char [10][10];
 
+    public Maze(){
+	Frontier A = new Frontier();
+	for(int row = 0; row < ary.length;row++){
+	    for(int col = 0; col < ary[row].length;col++){
+		A.addLast(new Coordinate(row, col));
+	    }
+	}
+	System.out.println(A.getLastCoordinate());
+    }
+
+    public static void main(String[]args){
+	Maze A = new Maze();
+    }
 
 }
