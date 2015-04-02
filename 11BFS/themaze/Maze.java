6d46287478a7c512c1a7c3c69964a82e5eaa6abb
Your Name <xwilliamy@gmail.com>
2015-04-02 13:10:19 -0400

    things looking good so far. Frontier class made to store possible moves, the beef of the problem will be addressed later

--- 11BFS/themaze/Maze.java
+++ 11BFS/themaze/Maze.java
@@ -2,13 +2,17 @@ public class Maze{
     private char [][] ary = new char [10][10];
 
     public Maze(){
+	ary[0][0] = 'x';
 	Frontier A = new Frontier();
 	for(int row = 0; row < ary.length;row++){
 	    for(int col = 0; col < ary[row].length;col++){
-		A.addLast(new Coordinate(row, col));
+		if(ary[row][col] != 'x'){		
+		    A.addLast(new Coordinate(row, col));
+		    System.out.println(A.getLastCoordinate());
+		}
 	    }
 	}
-	System.out.println(A.getLastCoordinate());
+	System.out.println(A.getFirstCoordinate());
     }
 
     public static void main(String[]args){
