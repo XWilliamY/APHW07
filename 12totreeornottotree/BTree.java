1cad64afa52c90fa0ba86d9e9c03153449b943b3
Your Name <xwilliamy@gmail.com>
2015-04-23 12:40:25 -0400

    eyy Btree or bsquare

---
+++ 12totreeornottotree/BTree.java
@@ -0,0 +1,7 @@
+import java.io.*;
+import java.util.*;
+
+public class BTree<E> {
+    public static final int PRE_ORDER = 0;
+    public static final int IN_ORDER = 1;
+    public static final int POST_ORDER = 2;
