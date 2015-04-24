3acda957d3fc4147c2a5165d351e5f5fe6a90f4c
Your Name <xwilliamy@gmail.com>
2015-04-24 12:58:09 -0400

    non-random adding functional

--- 12totreeornottotree/BTree.java
+++ 12totreeornottotree/BTree.java
@@ -14,38 +14,49 @@ public class BTree<E> {
     }
 
     public void add(E d){
-	root.setValue(d);
+	if(root == null){
+	    root = new TreeNode<E> (d);
+	    System.out.println(root.getValue());
+	}
+	else{
+	    TreeNode<E> bn = new TreeNode<E> (d);
+	    add(root, bn);
+	}
     }
 
     private void add( TreeNode<E> curr, TreeNode<E> bn){
-	root = curr;
 	if(curr.howFull() < 2){
 	    if(curr.howFull() == 0){
 		//randomly add here
 		curr.setRight(bn);
+		System.out.println(curr.getRight().getValue());
 	    }
 	    else if(curr.emptyRight()){
 		curr.setRight(bn);
+		System.out.println(curr.getRight().getValue());
 	    }
 	    else if(curr.emptyLeft()){
 		curr.setLeft(bn);
+		System.out.println(curr.getLeft().getValue());
 	    }
 	}
 	else{
 	    add(curr.getRight(), bn);
+	    System.out.println(curr.getRight().getValue());
 	}
     }
-    /*
-    public void traverse(int mode){
-	if(mode == PRE_ORDER){
-	    preOrder(root);
-	}
-	else if(mode == IN_ORDER){
-	    inOrder(root);
-	}
-	else{
-	    postOrder(root);
-	}
-	System.out.println();
-	}*/
+
+    public static void main(String[]args){
+	BTree A = new BTree();
+	/*
+	System.out.println("null: ");
+	A.add(new Integer(5));
+	System.out.println("the bn: ");
+	A.add(new Integer(10));
+	System.out.println("the bn after:");
+	A.add(new Integer(154));
+	A.add(new Integer(200));
+	*/
+
+    }
 }
