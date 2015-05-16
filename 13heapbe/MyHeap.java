06aa73cb63e3d87c931488d593081953c7a72652
William <xwilliamy@gmail.com>
2015-05-16 11:18:46 -0400

    Not sure if it works or not

--- 13heapbe/MyHeap.java
+++ 13heapbe/MyHeap.java
@@ -33,9 +33,9 @@ public class MyHeap{
 	    //2* index = left child, 2* index + 1 = right child
 	    heap[index] = n;
 	    //check if left or right
-	    if(index % 2 == 0 && index != 1){
+	    int tempIndex = index;
+	    if(index % 2 == 0){
 		System.out.println("this is the index: " + index);
-		int tempIndex = index;
 		while(heap[tempIndex/2] < heap [tempIndex] && tempIndex/2 != 0){
 		    //while the one before is less than this one
 		    int temp = heap[tempIndex/2];
@@ -46,12 +46,18 @@ public class MyHeap{
 		    heap[tempIndex] = temp;
 		    tempIndex = tempIndex/2;
 		}
-	    }
+	    }//above swaps left side
 	    else{
-		//is right
-		int tempIndex = index;
-		while(heap[temp
-	    index += index;
+		int tempIndexHalf = (tempIndex-1)/2;
+		//pretend that index = index - 1
+		while(heap[(tempIndexHalf)] < heap[tempIndex] && (tempIndexHalf != 0)){
+		    int temp = heap[tempIndexHalf];
+		    heap[tempIndexHalf] = heap[tempIndex];
+		    heap[tempIndex] = temp;
+		    tempIndex = tempIndexHalf;
+		}
+	    }
+	    index += 1;
 	    System.out.println("this is the new index: " + index);
 	    size++;
 	    resize();
@@ -62,12 +68,10 @@ public class MyHeap{
 	return heap[1];
     }
 
-    //borrowed Brandon's toString, will write my own after getting add and remove done
-
     public String toString(){
 	String ans = "";
 	for(int i = 0; i < heap.length; i++){
-	    ans += heap[i];
+	    ans += heap[i]+ " ";
 	}
 	return ans;
     }
@@ -77,18 +81,10 @@ public class MyHeap{
 	//index at 1
 	A.add(1); //index at 2
 	A.add(2); //index at 4
-	System.out.println(A.toString());
-	System.out.println("NEW ADD");
 	A.add(3);
-	System.out.println(A.toString());
-	System.out.println(A.peek());
-	System.out.println("NEW ADD");
 	A.add(4);
+	A.add(10);
 	System.out.println(A.toString());
-	System.out.println(A.peek());
-	A.add(5);
-	A.add(6);
-	A.add(7);
 	System.out.println(A.peek());
     }
 }
