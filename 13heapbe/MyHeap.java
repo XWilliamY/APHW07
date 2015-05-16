736f056fb73abfa779c8f84c90d96b9747106749
William <xwilliamy@gmail.com>
2015-05-16 11:00:16 -0400

    I feel like I'm piecing these things together with duct tape

--- 13heapbe/MyHeap.java
+++ 13heapbe/MyHeap.java
@@ -2,7 +2,7 @@ public class MyHeap{
     
     private int [] heap;
     private boolean isMax, isEmpty, leftEmpty, rightEmpty;
-    private int index;
+    private int index, size;
     
     public MyHeap(){
 	heap = new int [10];
@@ -19,11 +19,13 @@ public class MyHeap{
     }
 
     public void resize(){
-	int [] newHeap = new int [heap.length *2];
-	for(int i = 0; i < heap.length; i++){
-	    newHeap[i] = heap[i];
+	if(size == heap.length){
+	    int [] newHeap = new int [heap.length *2];
+	    for(int i = 0; i < heap.length; i++){
+		newHeap[i] = heap[i];
+	    }
+	    heap = newHeap;
 	}
-	heap = newHeap;
     }
 
     public void add(int n){
@@ -34,14 +36,23 @@ public class MyHeap{
 	    heap[index] = n;
 	    //check if left or right
 	    if(index % 2 == 0 && index != 1){
-		while(heap[index/2] < heap [index]){
+		System.out.println("this is the index: " + index);
+		int tempIndex = index;
+		while(heap[tempIndex/2] < heap [tempIndex] && tempIndex/2 != 0){
 		    //while the one before is less than this one
-		    int temp = heap[index/2];
-		    heap[index/2] = heap[index];
-		    heap[index] = temp;
+		    int temp = heap[tempIndex/2];
+		    System.out.println("this is temp: " + temp);
+		    System.out.println("this is index/2: " + tempIndex/2);
+		    System.out.println("this is index: " + tempIndex);
+		    heap[tempIndex/2] = heap[tempIndex];
+		    heap[tempIndex] = temp;
+		    tempIndex = tempIndex/2;
 		}
 	    }
-	    index = index*2;
+	    index += index;
+	    System.out.println("this is the new index: " + index);
+	    size++;
+	    resize();
 	}
     }
 
@@ -52,27 +63,26 @@ public class MyHeap{
     //borrowed Brandon's toString, will write my own after getting add and remove done
 
     public String toString(){
-	String s = "";
-	int power = 1;
-	for(int i = 1; i <= heap[0]; i++){
-	    if((int)Math.pow(2,power) == i){
-		s += "\n";
-		power++;
-	    }
-	    s += heap[i] + " ";
+	String ans = "";
+	for(int i = 0; i < heap.length; i++){
+	    ans += heap[i];
 	}
-	return s;
+	return ans;
     }
-
-
 
     public static void main(String[]args){
 	MyHeap A = new MyHeap();
-	A.add(5);
-	A.add(6);
-	A.add(7);
-	A.add(10);
-	//System.out.println(A.toString());
+	//index at 1
+	A.add(1); //index at 2
+	A.add(2); //index at 4
+	System.out.println(A.toString());
+	System.out.println("NEW ADD");
+	A.add(3);
+	System.out.println(A.toString());
+	System.out.println(A.peek());
+	System.out.println("NEW ADD");
+	A.add(4);
+	System.out.println(A.toString());
 	System.out.println(A.peek());
     }
 }
